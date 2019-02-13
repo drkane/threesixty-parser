b876f8deb702f955dc141caaf07b1ddbf96e91b0
David Kane <d.r.kane@gmail.com>
2019-02-13 22:30:09 +0000

    update setup config

--- setup.py
+++ setup.py
@@ -1,3 +1,17 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(name="THREESIXTYGIVING", packages=find_packages())
+setup(
+    name="threesixtygiving",
+    packages=["threesixty"],
+    version="0.0.2",
+    author="David Kane david@dkane.net",
+    include_package_data=True,
+    license="MIT",
+    description="A class to help use threesixtygiving data",
+    install_requires=[
+        "requests==2.19.1",
+        "jsonref==0.2",
+        "jsonschema==2.6.0",
+        "-e git+https://github.com/OpenDataServices/flatten-tool.git@bb485e6390ba5bed527546f8493a7a0b727fb354#egg=flattentool",
+    ]
+)
